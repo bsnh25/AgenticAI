100dde2d6f7697eacb6f985851f3fd9453e92888
Bayu Septyan Nur Hidayat <Bayu.Septyan@bni.co.id>
2026-06-30 14:04:23 +0700

    fix(android): remove compose compiler from non-UI modules

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(deps.plugins.android.library)
     alias(deps.plugins.kotlin.android)
     alias(deps.plugins.ksp)
-    alias(deps.plugins.compose.compiler)
+    
 }
 
 android {
