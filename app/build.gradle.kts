afb971fbec3866b3d8112e3cbe574f6bd824f697
Bayu Septyan Nur Hidayat <Bayu.Septyan@bni.co.id>
2026-06-30 13:56:51 +0700

    fix(android): revert to kotlinOptions to fix Unresolved reference compilerOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     buildFeatures {
